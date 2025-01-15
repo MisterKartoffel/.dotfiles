5f5a15fe8e5276a22fab7eabbf4887515ff2c702
Felipe Duarte <felipesdrs@hotmail.com>
2025-01-15 16:03:35 -0300

    feat(nvim/snacks.statuscolumn): Added and configured snacks.statuscolumn

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -48,5 +48,12 @@ return {
             exclude = { "markdown", },
         },
         scratch = { enabled = true, },
+        statuscolumn = {
+            enabled = true,
+            folds = {
+                open = true,
+                git_hl = true,
+            },
+        },
     },
 }
