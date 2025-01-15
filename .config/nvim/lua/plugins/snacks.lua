64aa6c21ebf6b0bac4a17aef07abf93f91da2ee6
Felipe Duarte <felipesdrs@hotmail.com>
2025-01-15 13:40:29 -0300

    feat(nvim/alpha->snacks.dashboard): Added and configured snacks.dashboard

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,27 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        dashboard = {
+            enabled = true,
+            preset = {
+                pick = "fzf-lua",
+                header = [[
+      ████ ██████           █████      ██                    
+     ███████████             █████                            
+     █████████ ███████████████████ ███   ███████████  
+    █████████  ███    █████████████ █████ ██████████████  
+   █████████ ██████████ █████████ █████ █████ ████ █████  
+ ███████████ ███    ███ █████████ █████ █████ ████ █████ 
+██████  █████████████████████ ████ █████ █████ ████ ██████]]
+            },
+            sections = {
+                { section = "header" },
+                { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+                { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+                { section = "startup" },
+            },
+        },
+    },
+}
