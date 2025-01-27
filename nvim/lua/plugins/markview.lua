e2ad4ba3ff2c6d30f9ffe76bd33b51563b6a5f9a
Felipe Duarte <felipesdrs@hotmail.com>
2025-01-27 17:13:55 -0300

    feat(nvim/markview): Updated configuration for version 0.25

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -1,18 +1,19 @@
 return {
-    "OXY2DEV/markview.nvim",
-    ft = "markdown",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-tree/nvim-web-devicons",
-    },
+	"OXY2DEV/markview.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
 
-    config = function()
-        local markview = require("markview")
-        local presets = require("markview.presets")
+	config = function()
+		local markview = require("markview")
+		local presets = require("markview.presets")
 
-        markview.setup({
-            checkboxes = presets.checkboxes.nerd,
-            headings = presets.headings.simple,
-        })
-    end
+		markview.setup({
+			headings = presets.headings.glow,
+			checkboxes = presets.checkboxes.nerd,
+			tables = presets.tables.rounded,
+		})
+	end,
 }
