6331a25a17e35e47fc39498bd9418206bc93aa32
Felipe Duarte <felipesdrs@hotmail.com>
2025-03-27 22:36:01 -0300

    feat(nvim/0.11/LSP): Added yamlls LSP setup

---
+++ nvim/lsp/yamlls.lua
@@ -0,0 +1,11 @@
+return {
+    cmd = { "yaml-language-server", "--stdio" },
+    filetypes = { "yaml" },
+    settings = {
+        redhat = {
+            telemetry = {
+                enabled = false,
+            },
+        },
+    },
+}
