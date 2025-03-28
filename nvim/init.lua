cfcbf224c58b9e3a01620c9352f9d63ccbc1eefe
Felipe Duarte <felipesdrs@hotmail.com>
2025-03-27 22:34:57 -0300

    feat(nvim/0.11/LSP): Added boilerplate init.lua configuration for 0.11.0 LSP setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,27 @@ require("core.lazy")
 require("core.options")
 require("core.keybindings")
 require("core.autocmd")
+
+-- LSP setup for servers configured in lsp/*
+local configs = {}
+
+for _, file in ipairs(vim.api.nvim_get_runtime_file("lsp/*.lua", true)) do
+    local name = vim.fn.fnamemodify(file, ":t:r")
+    configs[name] = true
+end
+
+vim.lsp.config("*", {
+    root_markers = { ".git" },
+})
+
+vim.diagnostic.config({
+    virtual_lines = {
+        current_line = true,
+        format = function(diagnostic)
+            return diagnostic.message
+        end,
+    },
+})
+
+vim.lsp.enable(vim.tbl_keys(configs))
+vim.diagnostic.enable()
