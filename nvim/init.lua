82495509d15bc15736a3996ece832d7825b2e99f
Felipe Duarte <felipesdrs@hotmail.com>
2025-03-28 00:56:12 -0300

    feat(nvim/0.11/LSP): Moved LSP configuration to plugin path

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,5 @@
--- LSP setup for servers configured in lsp/*
-local configs = {}
-
-for _, file in ipairs(vim.api.nvim_get_runtime_file("lsp/*.lua", true)) do
-    local name = vim.fn.fnamemodify(file, ":t:r")
-    configs[name] = true
 end
 
-vim.lsp.config("*", {
-    root_markers = { ".git" },
-})
 
-vim.diagnostic.config({
-    virtual_lines = {
-        current_line = true,
-        format = function(diagnostic)
-            return diagnostic.message
-        end,
     },
 })
-
-vim.lsp.enable(vim.tbl_keys(configs))
-vim.diagnostic.enable()
---
+++ nvim/plugin/lsp.lua
@@ -0,0 +1,85 @@
+-- LSP setup for servers configured in lsp/*
+local configs = {}
+
+for _, file in ipairs(vim.api.nvim_get_runtime_file("lsp/*.lua", true)) do
+    local name = vim.fn.fnamemodify(file, ":t:r")
+    configs[name] = true
+end
+
+vim.lsp.config("*", {
+    root_markers = { ".git" },
+})
+
+vim.lsp.enable(vim.tbl_keys(configs))
+
+vim.diagnostic.config({
+    virtual_lines = {
+        current_line = true,
+        format = function(diagnostic)
+            return diagnostic.message
+        end,
+    }
+})
+
+vim.diagnostic.enable()
+
+local function map(mode, lhs, rhs, opts)
+    local default_opts = { silent = true }
+    if opts then
+        opts = vim.tbl_extend("force", default_opts, opts)
+    else
+        opts = default_opts
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("LSP", { clear = false }),
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+        if client:supports_method("textDocument/codeAction") then
+            map("n", "<leader>la", vim.lsp.buf.code_action,
+                { desc = "Displays [L]SP [a]ctions for annotation under cursor" })
+        end
+
+        if client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, args.buf, {})
+            map("i", "<C-Space>", vim.lsp.completion.get, { desc = "Display [L]SP [c]ompletions" })
+        end
+
+        if client:supports_method("textDocument/definition") then
+            map("n", "<leader>ld", vim.lsp.buf.definition,
+                { desc = "Jump to [L]SP [d]efinition for symbol under cursor" })
+        end
+
+        -- [TODO]: Find out how to move diagnostic stuff from init.lua into here
+        if client:supports_method("textDocument/diagnostic") then
+            map("n", "<leader>le", vim.diagnostic.enable(not vim.diagnostic.is_enabled()),
+                { desc = "Toggle [L]SP [v]irtual line diagnostics" })
+        end
+
+        if client:supports_method("textDocument/formatting") then
+            map("n", "<leader>lf", vim.lsp.buf.format, { desc = "[L]SP auto[f]ormatting" })
+        end
+
+        if not client:supports_method("textDocument/hover") then
+            map("n", "K", "<NOP>", { desc = "Disable [L]SP [h]over if unavailable" })
+        end
+
+        if client:supports_method("textDocument/implementation") then
+            vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation,
+                { desc = "List [L]SP [i]mplementations for currently hovered symbol" })
+        end
+
+        if not client:supports_method("textDocument/willSaveWaitUntil") and client:supports_method("textDocument/formatting") then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = vim.api.nvim_create_augroup("LSPformat", { clear = false }),
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+                end,
+            })
+        end
+    end,
+})
