0aa7a2b7134d0cdf21e3976d7c04882603c30fc7
Felipe Duarte <felipesdrs@hotmail.com>
2025-03-27 22:35:32 -0300

    feat(nvim/0.11/LSP): Added lua_ls LSP setup

---
+++ nvim/lsp/lua_ls.lua
@@ -0,0 +1,17 @@
+return {
+    cmd = { "lua-language-server" },
+    root_markers = { ".luarc.json", ".luarc.jsonc", },
+    filetypes = { "lua" },
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
+            workspace = {
+                library = {
+                    vim.env.VIMRUNTIME,
+                },
+            },
+        },
+    },
+}
