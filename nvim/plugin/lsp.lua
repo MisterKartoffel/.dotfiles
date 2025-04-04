4240b239293a01ba6d0c6d5e19e95aae7ec3294a
Felipe Duarte <felipesdrs@hotmail.com>
2025-04-03 23:16:51 -0300

    feat(nvim/LSP): Simplified vim.lsp.enable() calls

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,16 +1,12 @@
 -- LSP setup for servers configured in lsp/*
-local configs = {}
-
-for _, file in ipairs(vim.api.nvim_get_runtime_file("lsp/*.lua", true)) do
-    local name = vim.fn.fnamemodify(file, ":t:r")
-    configs[name] = true
-end
-
 vim.lsp.config("*", {
     root_markers = { ".git" },
 })
 
-vim.lsp.enable(vim.tbl_keys(configs))
+for _, file in ipairs(vim.api.nvim_get_runtime_file("lsp/*.lua", true)) do
+    local name = vim.fn.fnamemodify(file, ":t:r")
+    vim.lsp.enable(name)
+end
 
 vim.diagnostic.config({
     virtual_lines = {
