891784f30e84e1d6f5f7d1a04edc1f96d1abbdda
Felipe Duarte <felipesdrs@hotmail.com>
2025-04-03 23:15:38 -0300

    fix(nvim/ftplugin): This is not as useful as I once thought

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -3,4 +3,3 @@ local map = require("utils").map
 map("n", "sib", "viWdi()<Esc>P", { desc = "Surround current word in parenthesis" })
 map("n", "siB", "viWdi{<Space><Space>}<Esc>hP", { desc = "Surround current word in curly brackets" })
 map("n", "siq", "viWdi\"\"<Esc>P", { desc = "Surround current word in quotes" })
-map("n", "sif", "viWdi()<Esc>PF(ifunction<Esc>", { desc = "Surround current argument (WORD) in a function call" })
