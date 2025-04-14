95ff79b06060e167c8cc752c3fb6f04c4aa5de59
Felipe Duarte <felipesdrs@hotmail.com>
2025-04-14 09:16:59 -0300

    fix(nvim/ftplugin): Changed description wording

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,5 +1,5 @@
 local map = require("utils").map
 
-map("n", "sib", "viWdi()<Esc>P", { desc = "Surround current word in parenthesis" })
-map("n", "siB", "viWdi{<Space><Space>}<Esc>hP", { desc = "Surround current word in curly brackets" })
-map("n", "siq", "viWdi\"\"<Esc>P", { desc = "Surround current word in quotes" })
+map("n", "sib", "viWdi()<Esc>P", { desc = "Surround current WORD in parenthesis" })
+map("n", "siB", "viWdi{<Space><Space>}<Esc>hP", { desc = "Surround current WORD in curly brackets" })
+map("n", "siq", "viWdi\"\"<Esc>P", { desc = "Surround current WORD in quotes" })
