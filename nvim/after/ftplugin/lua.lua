5872754952a9a1936165129201b3050f111a338e
Felipe Duarte <felipesdrs@hotmail.com>
2025-04-22 14:27:36 -0300

    feat(nvim/ftplugin): Simplified commands

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,5 +1,5 @@
 local map = require("utils").map
 
-map("n", "sib", "viWdi()<Esc>P", { desc = "Surround current WORD in parenthesis" })
-map("n", "siB", "viWdi{<Space><Space>}<Esc>hP", { desc = "Surround current WORD in curly brackets" })
-map("n", "siq", "viWdi\"\"<Esc>P", { desc = "Surround current WORD in quotes" })
+map("n", "sib", "diWa()<Esc>P", { desc = "Surround current WORD in parenthesis" })
+map("n", "siB", "diWa{<Space><Space>}<Esc>hP", { desc = "Surround current WORD in curly brackets" })
+map("n", "siq", "diWa\"\"<Esc>P", { desc = "Surround current WORD in quotes" })
