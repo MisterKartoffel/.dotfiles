d835e9f338d8817913ce7355ed87b97dd7799434
Felipe Duarte <felipesdrs@hotmail.com>
2025-05-12 20:46:15 -0300

    feat(nvim/ftplugin): Added bash filetype plugin

---
+++ nvim/after/ftplugin/bash.lua
@@ -0,0 +1,4 @@
+local map = require("utils").map
+
+map("n", "siB", "eF$a{<Esc>ea}<Esc>", { desc = "Surround current variable in braces" })
+map("n", "siq", "diWa\"\"<Esc>P", { desc = "Surround current WORD in quotes" })
