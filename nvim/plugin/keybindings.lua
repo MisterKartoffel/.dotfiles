5f0d1baca69ebfa142dc72d279d3733f54cd169b
Felipe Duarte <felipesdrs@hotmail.com>
2025-05-20 10:57:31 -0300

    feat(nvim): Changed J remap to not use marks

--- nvim/plugin/keybindings.lua
+++ nvim/plugin/keybindings.lua
@@ -9,7 +9,7 @@ local map = require("utils").map
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected string down a line and autoindent" })
 map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected string up a line and autoindent" })
 
-map("n", "J", "mzJ`z", { desc = "Append line below while keeping cursor still" })
+map("n", "J", "V<Esc>Jgv<Esc>zz", { desc = "Append line below while keeping cursor still" })
 map("n", "<C-d>", "<C-d>zz", { desc = "Scroll half page down and center screen on cursor" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Scroll half page up and center screen on cursor" })
 
