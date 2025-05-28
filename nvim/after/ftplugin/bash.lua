272123eb2e27bbbe2913f0470098ee32116417ba
Felipe Duarte <felipesdrs@hotmail.com>
2025-05-28 00:48:17 -0300

    feat(nvim/surround): Added mini.surround

--- nvim/after/ftplugin/bash.lua
+++
@@ -1,4 +0,0 @@
-local nmap = require("utils").nmap
-
-nmap("siB", "eF$a{<Esc>ea}<Esc>", { desc = "Surround current variable in braces" })
-nmap("siq", "diWa\"\"<Esc>P", { desc = "Surround current WORD in quotes" })
--- nvim/after/ftplugin/lua.lua
+++
@@ -1,5 +0,0 @@
-local nmap = require("utils").nmap
-
-nmap("sib", "diWa()<Esc>P", { desc = "Surround current WORD in parenthesis" })
-nmap("siB", "diWa{<Space><Space>}<Esc>hP", { desc = "Surround current WORD in curly brackets" })
-nmap("siq", "diWa\"\"<Esc>P", { desc = "Surround current WORD in quotes" })
