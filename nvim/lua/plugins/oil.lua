b3fc68c7f4ed32f4f0f5e87c1ed9935418dee6d1
Felipe Duarte <felipesdrs@hotmail.com>
2025-07-04 16:37:22 -0300

    feat!: read extended description
    System:
    - Removed yazi
    - Added Celluloid (flatpak)
    
    Neovim:
    - Removed yazi.nvim
    - Removed floaterm
    - Added oil.nvim

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", },
+    lazy = false,
+    opts = {
+        delete_to_trash = true,
+        view_options = { show_hidden = true, },
+    },
+}
