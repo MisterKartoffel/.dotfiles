528a0a21701ac6db9aae588c59ab9cbf68813ea2
Felipe Duarte <felipesdrs@hotmail.com>
2025-09-29 09:01:15 -0300

    chore(nvim/nvim-colorizer): removed redundant option

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -9,7 +9,6 @@ return {
         user_default_options = {
             RRGGBBAA = true,
             css = true,
-            css_fn = true,
             mode = "background",
             always_update = true,
         },
