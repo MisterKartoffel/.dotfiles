ae5626509910d026075a05b6f25890c15574acdf
Felipe Duarte <felipesdrs@hotmail.com>
2025-09-29 09:19:06 -0300

    fix(nvim/treesitter): changed to recommended branch/lazyloading

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    event = {
-        "BufReadPre",
-        "BufNewFile",
-    },
+    branch = "master",
+    lazy = false,
     build = ":TSUpdate",
     opts = {
         auto_install = true,
