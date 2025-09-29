91a1396d00100f725e64fcb1634085072e9c3567
Felipe Duarte <felipesdrs@hotmail.com>
2025-09-29 09:08:05 -0300

    chore(nvim/mason): also, this was the default anyway

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,12 +1,4 @@
 return {
     "mason-org/mason.nvim",
-    opts = {
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            },
-        },
-    },
+    config = true,
 }
