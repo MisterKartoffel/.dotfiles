cb0eef6a19e369797c18bc7281d9d68ad8026e4b
Felipe Duarte <felipesdrs@hotmail.com>
2025-10-20 20:53:36 -0300

    feat(scripts/pac): major rewrite
    TODO:
    - Wrap things in functions nicely
    - Handle error cases

--- scripts/helper/pac-footer.sh
+++ scripts/helper/pac-footer.sh
@@ -14,14 +14,26 @@
 # This is here because otherwise fzf hangs, don't ask
 exec < /dev/null
 
-LIST=${1}
+LIST=${*}
+STATE=$(<"/tmp/fzf_mode.txt")
+
+if [[ ${STATE} == 2 ]]; then
+    COLOR=$(tput setaf 1)
+    PACKAGE_NAME=2
+else
+    COLOR=$(tput setaf 4)
+    PACKAGE_NAME=1
+fi
 
 # Regex:
 # Captures a group wrapped in single quotes composed of
-# - String composed of 1 or more non-space characters (package name)
+# - String composed of 1 or more non-space characters
+#   with an optional space at the end (repository name)
+# - String composed of 1 or more non-space characters
+#   (package name)
 # - Optional space followed by "[installed]" string
 # With another optional space at the end (actual separator between entries)
-while [[ ${LIST} =~ \'([^[:space:]]+( \[installed\])?)\'[[:space:]]? ]]; do
-    echo "${BASH_REMATCH[1]}"
+while [[ ${LIST} =~ \'((([^[:space:]]+ )?[^[:space:]]+)( \[installed\])?)\'[[:space:]]? ]]; do
+    printf "${COLOR}%s\n" "${BASH_REMATCH[${PACKAGE_NAME}]}"
     LIST=${LIST#*"${BASH_REMATCH[0]}"}
 done
