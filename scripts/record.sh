d8b7c867e8162162f0f7feafa031c913bc7868cd
Felipe Duarte <felipesdrs@hotmail.com>
2025-12-25 20:24:02 -0300

    feat(scripts/record.sh)!: implied notify-send exists

--- scripts/record.sh
+++ scripts/record.sh
@@ -2,19 +2,19 @@
 
 REC_FILE="${HOME}/Videos/Screencaptures/$(date --rfc-3339="seconds").mkv"
 
-declare -a DEPENDS=(notify-send wf-recorder)
+declare -a DEPENDS=(wf-recorder)
 
 function check_depends() {
     for COMMAND in "${DEPENDS[@]}"; do
         if ! command -v "${COMMAND}" >/dev/null; then
-            echo "${COMMAND} is missing. Exiting."
+            notify-send --urgency=critical --app-name="record.sh" "wf-recorder" "${0}: missing dependency -- ${COMMAND}"
             exit 1
         fi
     done
 }
 
 function record() {
-    notify-send --expire-time=500 --app-name="wf-recorder" "wf-recorder" "Recording started"
+    notify-send --expire-time=500 --app-name="record.sh" "wf-recorder" "Recording started"
     wf-recorder -f "${REC_FILE}"
 }
 
@@ -22,7 +22,7 @@ function end() {
     kill -15 "$(pidof wf-recorder)" 2>/dev/null || true
     wait "$(pidof wf-recorder)" 2>/dev/null || true
 
-    notify-send --expire-time=500 --app-name="wf-recorder" "wf-recorder" "Recording stopped"
+    notify-send --expire-time=500 --app-name="record.sh" "wf-recorder" "Recording stopped"
 }
 
 check_depends
